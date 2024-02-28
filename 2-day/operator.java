091508ad1d3dee637b28dd43c9e0bf7e7d659621
Dhruv <dhruv.verma8@gmail.com>
2024-02-28 23:48:23 +0530

    day 2

---
+++ 2-day/operator.java
@@ -0,0 +1,37 @@
+
+public class operator {
+    public static void main(String[] args) {
+        int a=20;
+        int b=10;
+
+        //arthmetic operator
+        System.out.println(a + b);
+        System.out.println(a - b);
+        System.out.println(a * b);
+        System.out.println(a / b);
+        System.out.println(a % b);
+        
+        //pre Increment
+        int c = ++a;
+        System.out.println(a);
+        System.out.println(c);
+
+        //post Increment
+        int d = a--;
+        System.out.println(a);
+        System.out.println(d);
+    }
+}
+
+// type of operator
+
+//     arthmetic operator
+            // Binary { + , - , * , / , % }
+            // Unary  { ++ ,  -- }
+                    // pre Increment eg:- ++a  1st (value change) 2nd (vlaue use)
+                    // post Increment eg:- a++ 1st (vlaue use) 2nd (value change)
+//     Relational operator
+//     Logical operator
+//     Bitwise operator
+//     Assingnment operator
+
