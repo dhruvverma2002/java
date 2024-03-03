3d71af8c1dd24d15f32b3738f64fbb1021807cd5
Dhruv <dhruv.verma8@gmail.com>
2024-03-04 00:05:29 +0530

    Operators

--- 2-day/operator.java
+++ 2-day/operator.java
@@ -4,52 +4,61 @@ public class operator {
         int a=20;
         int b=10;
 
+        System.out.println("Arthmetic operator");
         //arthmetic operator
-        System.out.println(a + b);
-        System.out.println(a - b);
-        System.out.println(a * b);
-        System.out.println(a / b);
-        System.out.println(a % b);
+        System.out.println("a + b :- " + (a + b));
+        System.out.println("a - b :- " + (a - b));
+        System.out.println("a * b :- " + (a * b));
+        System.out.println("a / b :- " + (a / b));
+        System.out.println("a % b :- " + (a % b));
         
+        System.out.println("Pre Increment");
         //pre Increment
         int c = ++a;
-        System.out.println(a);
-        System.out.println(c);
+        System.out.println("a:- " + a);
+        System.out.println("c:- " + c);
 
+        System.out.println("Post Increment");
         //post Increment
         int d = a--;
-        System.out.println(a);
-        System.out.println(d);
+        System.out.println("a:- " + a);
+        System.out.println("d:- " + d);
 
+        System.out.println("\nRelational operator\n");
         // Relational operator
-        System.out.println((a==b));
-        System.out.println((a!=b));
-        System.out.println((a>b));
-        System.out.println((a<b));
-        System.out.println((a>=b));
-        System.out.println((a<=b));
+        System.out.println("(a==b):- " + (a==b));
+        System.out.println("(a!=b):- " + (a!=b));
+        System.out.println("(a>b):- " + (a>b));
+        System.out.println("(a<b):- " + (a<b));
+        System.out.println("(a>=b):- " + (a>=b));
+        System.out.println("(a<=b):- " + (a<=b));
 
+        System.out.println("\nLogical operator\n");
         // Logical operator { && , || , ! }
         // &&
-        System.out.println(true && true);
-        System.out.println(true && false);
-        System.out.println(false && true);
-        System.out.println(false && false);
+        System.out.println("true && true:- " + (true && true));
+        System.out.println("true && false:- " + (true && false));
+        System.out.println("false && true:- " + (false && true));
+        System.out.println("false && false:- " + (false && false));
+        System.out.println();
         // ||
-        System.out.println(true || true);
-        System.out.println(true || false);
-        System.out.println(false || true);
-        System.out.println(false || false);
+        System.out.println("true || true:- " + (true || true));
+        System.out.println("true || false:- " + (true || false));
+        System.out.println("false || true:- " + (false || true));
+        System.out.println("false || false:- " + (false || false));
+        System.out.println();
         // !
-        System.out.println(!true);
-        System.out.println(!false);
+        System.out.println("!true:- " + !true);
+        System.out.println("!false:- " + !false);
 
+        
+        System.out.println("\nAssingnment operator\n");
         // Assingnment operator
-        System.out.println(a=b);
-        System.out.println(a+=b);
-        System.out.println(a-=b);
-        System.out.println(a*=b);
-        System.out.println(a/=b);
+        System.out.println("a=b:- " + (a=b));
+        System.out.println("a+=b:- " + (a+=b));
+        System.out.println("a-=b:- " + (a-=b));
+        System.out.println("a*=b:- " + (a*=b));
+        System.out.println("a/=b:- " + (a/=b));
 
     }
 }
