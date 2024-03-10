18212588039be02105800f821bbc182f54c262d5
Dhruv Verma <46262886+dhruvverma2002@users.noreply.github.com>
2024-03-10 23:58:10 +0530

    Loops

--- 4-day/forLoop.java
+++ 4-day/forLoop.java
@@ -3,8 +3,7 @@ public class forLoop{
         Scanner s = new Scanner(System.in);
             System.out.print("N :- " );
             int n =  s.nextInt();
-            
-        for (int i = 1; i <= n;i++) {
+            for (int i = 1; i <= n;i++) {
             System.out.print(counter++ + " ");
         }
     }
